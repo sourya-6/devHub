62de6eedb3f78b264d031c604b98c4c1c8c05853
sourya-6 <saisourya678@gmail.com>
2026-05-27 16:25:37 +0530

    added the nest backendUrl

--- frontendAngular/src/environments/environment.ts
+++ frontendAngular/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  backendUrl: "http://localhost:3000",
-  // backendUrl: "https://devhub-nsqb.onrender.com",  // Production backend
+  // backendUrl: "http://localhost:3000",
+  backendUrl: "https://devhub-1-53fw.onrender.com",  // Production backend
   googleClientId: "424192437084-nfbhpiheprpa4ns7g5fasohv05fe83g2.apps.googleusercontent.com",
 };
 
